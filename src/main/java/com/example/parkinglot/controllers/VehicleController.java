364df4f3a159893bb77da47a6da8cb173148a39c
namdt <thanhnamak@gmail.com>
2026-02-04 16:06:39 +0700

    ex 2

--- src/main/java/com/example/parkinglot/controllers/VehicleController.java
+++ src/main/java/com/example/parkinglot/controllers/VehicleController.java
@@ -1,15 +1,14 @@
 package com.example.parkinglot.controllers;
 
 import com.example.parkinglot.dtos.ApiResponse;
+import com.example.parkinglot.dtos.PageResponse;
 import com.example.parkinglot.dtos.VehicleCreateRequest;
 import com.example.parkinglot.dtos.VehicleResponse;
 import com.example.parkinglot.services.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/vehicles")
@@ -29,6 +28,25 @@ public class VehicleController {
             return ResponseEntity
                 .status(201)
                 .body(ApiResponse.success("Vehicle created successfully", vehicleService.createVehicle(req)));
+        } catch (Exception e) {
+            return ResponseEntity
+                .status(400)
+                .body(ApiResponse.error(e.getMessage()));
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<PageResponse<VehicleResponse>>> getPagedVehicles(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) String sortBy,
+            @RequestParam(required = false) Sort.Direction direction,
+            @RequestBody(required = false) String keyword
+    ) {
+        try {
+            return ResponseEntity
+                .ok()
+                .body(ApiResponse.success("Vehicles fetched successfully", vehicleService.getPagedVehicles(page, size, sortBy, direction, keyword)));
         } catch (Exception e) {
             return ResponseEntity
                 .status(400)
--- src/main/java/com/example/parkinglot/services/VehicleService.java
+++ src/main/java/com/example/parkinglot/services/VehicleService.java
@@ -1,10 +1,15 @@
 package com.example.parkinglot.services;
 
+import com.example.parkinglot.dtos.PageResponse;
 import com.example.parkinglot.dtos.VehicleCreateRequest;
 import com.example.parkinglot.dtos.VehicleResponse;
 import com.example.parkinglot.models.Vehicle;
 import com.example.parkinglot.repos.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,5 +27,16 @@ public class VehicleService {
         vehicle.setColor(req.getColor());
         vehicle.setVehicleType(req.getVehicleType());
         return new VehicleResponse(repo.save(vehicle));
+    }
+
+    public PageResponse<VehicleResponse> getPagedVehicles(Integer page, Integer size, String sortBy, Sort.Direction direction, String keyword) {
+        Sort sort = Sort.unsorted();
+        if (sortBy != null && direction != null) {
+            sort = Sort.by(direction, sortBy);
+        }
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<VehicleResponse> vehiclePage = repo.findAllByKeyword(keyword, pageable);
+
+        return PageResponse.from(vehiclePage);
     }
 }
